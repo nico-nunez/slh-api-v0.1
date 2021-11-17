c77693b70d2c93f174b5a92372ccdacab33dbe2d
incogNic0 <incognico.dev@gmail.com>
2021-11-16 22:08:40 -0600

    add edit page and route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+const methodOverride = require('method-override');
 
 // --------- Models -----------
 const List = require('./models/list');
@@ -19,7 +20,8 @@ db.once("open", () => {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 
 app.get('/', (req, res) => {
@@ -30,9 +32,10 @@ app.get('/lists', async (req, res) => {
     const lists = await List.find({})
     res.render('lists/index', {lists});
 });
+
 app.post('/lists', async (req, res) => {
     const { list } = req.body;
-    const newList = new List({title: list.title});
+    const newList = new List({...list});
     await newList.save();
     res.redirect(`lists/${newList._id}`);
 })
@@ -44,6 +47,17 @@ app.get('/lists/new', (req, res) => {
 app.get('/lists/:id', async (req, res) => {
     const list = await List.findById(req.params.id)
     res.render('lists/show', {list})
+})
+
+app.put('/lists/:id', async(req, res) => {
+    const { id } = req.params;
+    await List.findByIdAndUpdate(id, {...req.body.list});
+    res.redirect(`/lists/${id}`);
+});
+
+app.get('/lists/:id/edit', async (req, res) => {
+    const list = await List.findById(req.params.id);
+    res.render('lists/edit', { list });
 })
 
 
