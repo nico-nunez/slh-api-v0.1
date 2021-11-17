d04d8daef6b6c72854dde8702ecc67f257bba57a
incogNic0 <incognico.dev@gmail.com>
2021-11-16 19:01:05 -0600

    add ejs view engine and views folder w/ index.ejs

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express();
+const path = require('path');
+
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!!!!!');
+    res.render('index');
 });
 
 app.listen(PORT, () => {
