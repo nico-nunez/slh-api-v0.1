756dc00d9de2757abee5c685f60dec7137b64ee0
incogNic0 <incognico.dev@gmail.com>
2021-11-16 19:59:59 -0600

    add mongoose connection

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
 
-const PORT = 8080;
+// -------------- Mongoose -----------
+const mongoDBUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/grab-bag';
+
+const db = mongoose.connection;
+mongoose.connect(mongoDBUrl, 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    });
+
+db.on("error", console.error.bind(console, 'Mongo Connection Failed...'));
+db.once("open", () => {
+    console.log("Mongo Connection Open...");
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
 })
