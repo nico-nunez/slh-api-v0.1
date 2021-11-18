d6e8d986c10813400f35b7eb7e63faee011b00fb
incogNic0 <incognico.dev@gmail.com>
2021-11-18 06:52:43 -0600

    add public folder. rename landing view from index to home. minor stylings

--- app.js
+++ app.js
@@ -25,9 +25,11 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('home');
 });
 
 app.get('/lists', async (req, res) => {
