0b302d8473f571a32da47509d61d6a55438e7df4
incogNic0 <incognico.dev@gmail.com>
2021-11-19 15:21:56 -0600

    add login auth

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const passport = require('passport');
 const days = require('dayjs');
 const { ExpressError, catchAsync } = require('../utils');
 // const { validateUser } = require('../joiSchemas');
@@ -11,27 +12,31 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', catchAsync (async (req, res) => {
-    const { username, profileName, password, confirmPass } = req.body.newUser;
-    if(password !== confirmPass) {
-        req.flash('error', 'Passwords must match.')
-        return res.redirect('/users/register')
+    try {
+        const { username, profileName, password, confirmPass } = req.body.newUser;
+        if(password !== confirmPass) {
+            req.flash('error', 'Passwords must match.')
+            return res.redirect('/users/register')
+        }
+        const newUser = new User({ username, profileName });
+        const user = await User.register(newUser, password);
+        req.flash('success', 'Welcome! Thank you for joining!');
+        res.redirect('/parties');
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('users/register');
     }
-    const newUser = new User({ username, profileName });
-    const user = await User.register(newUser, password);
-    req.flash('success', 'Welcome! Thank you for joining!');
-    res.redirect('/parties');
+
 }));
 
 router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
-router.post('/login', catchAsync (async (req, res) => {
-    const { user } = req.body;
-    // const newUser = new User({email: '', username: 'Fakey McFakester', profileName: 'fake_son'});
-    // const user = await User.register(newUser, 'fakePass');
-    res.send(user);
-}));
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/users/login'}), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/parties');
+});
 
 router.get('/register', (req, res) => {
     res.render('users/register');
