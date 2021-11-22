cb472b2a6317c7f97d5a9530fe7e2a4f7b8c13d8
incogNic0 <incognico.dev@gmail.com>
2021-11-22 13:30:49 -0600

    add icon to lists. add name/password change forms

---
+++ public/js/users.js
@@ -0,0 +1,19 @@
+const editNameLink = document.querySelector('#edit-name-link')
+const changePasswordLink = document.querySelector('#change-pass-link');
+
+editNameLink.addEventListener('click', handleEditNameClick );
+changePasswordLink.addEventListener('click', handleChangePasswordClick );
+
+
+function handleEditNameClick(evt) {
+    evt.preventDefault()
+    document.querySelector('#change-password').classList.add('hidden');
+    document.querySelector('#edit-name').classList.toggle('hidden');
+}
+
+function handleChangePasswordClick(evt) {
+    evt.preventDefault();
+    document.querySelector('#change-password').classList.toggle('hidden');
+    document.querySelector('#edit-name').classList.add('hidden')
+}
+
