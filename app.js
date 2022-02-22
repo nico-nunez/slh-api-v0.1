373f92ffcfa549641731bf38907f8c9d45d42821
incogNic0 <incognico.dev@gmail.com>
2022-02-21 19:51:08 -0600

    add secure cookies for prod, and favicon

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
+const path = require('path');
 const express = require('express')
 const app = express();
-const path = require('path');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const session = require('express-session');
@@ -12,10 +12,9 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const favicon = require('serve-favicon');
 const passportConfig = require('./middleware/passport');
 const { connectDB, sessionConfig } = require('./helpers/configs');
-
-
 const { ExpressError, errorHandler } = require('./helpers/errors');
 
 connectDB();
@@ -26,6 +25,9 @@ const partiesRoutes = require('./routes/parties.router');
 const usersRoutes = require('./routes/users.router');
 const authRoutes = require('./routes/auth.router');
 
+if (process.env.NODE_ENV === 'production'){
+  app.set('trust proxy', 1)
+}
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -33,9 +35,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(methodOverride('_method'));
+app.use(session(sessionConfig));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize())
-app.use(session(sessionConfig));
 app.use(flash());
 app.use(helmet({contentSecurityPolicy: false}));
 
@@ -60,9 +62,8 @@ app.use('/lists', listsRoutes);
 app.use('/parties', partiesRoutes);
 app.use('/users', usersRoutes);
 app.use('/auth', authRoutes);
+app.use(favicon(path.join(__dirname, '/public/assets/santa-hat.png')));
 
-
-app.get('/favicon.ico', (req, res) => res.status(204));
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404));
 })
