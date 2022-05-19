d03d20f554c1c5896df0eca66ff90d5a866d2ac4
incogNic0 <incognico.dev@gmail.com>
2022-05-19 07:33:07 -0500

    update cookie options

--- helpers/configs.js
+++ helpers/configs.js
@@ -4,41 +4,39 @@ const MongoStore = require('connect-mongo');
 const mongoDBUrl = process.env.MONGODB_URL;
 const secret = process.env.SECRET;
 
-
 const db = mongoose.connection;
 
 const connectDB = () => {
-  mongoose.connect(mongoDBUrl);
+	mongoose.connect(mongoDBUrl);
 
-  db.on("error", console.error.bind(console, 'Mongo Connection Failed...'));
-  db.once("open", () => {
-      console.log("Mongo Connection Open...");
-  });
-}
-
+	db.on('error', console.error.bind(console, 'Mongo Connection Failed...'));
+	db.once('open', () => {
+		console.log('Mongo Connection Open...');
+	});
+};
 
 const store = MongoStore.create({
-  mongoUrl: mongoDBUrl,
-  touchAfter: 24*60*60,
-  crypto: {
-    secret 
-  }
+	mongoUrl: mongoDBUrl,
+	touchAfter: 24 * 60 * 60,
+	crypto: {
+		secret,
+	},
 });
 
 store.on('error', function (e) {
-  console.log(('SESSION STORE ERROR', e))
+	console.log(('SESSION STORE ERROR', e));
 });
 
 const sessionConfig = {
-  store,
-  secret,
-  name: 'appSession',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    expires: 1000 * 60 * 60 * 24 * 7 * 2,
-  }
-}
+	store,
+	secret,
+	name: 'appSession',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		// secure: process.env.NODE_ENV === 'production',
+		expires: 1000 * 60 * 60 * 24 * 7 * 2,
+	},
+};
 
 module.exports = { connectDB, sessionConfig };
