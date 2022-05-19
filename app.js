d0dc83c83063ba71ab5e6f0a461d50b4416c8efc
incogNic0 <incognico.dev@gmail.com>
2022-05-18 22:04:25 -0500

    update for heroku deploy

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
 }
 
 const path = require('path');
-const express = require('express')
+const express = require('express');
 const app = express();
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
@@ -25,20 +25,20 @@ const partiesRoutes = require('./routes/parties.router');
 const usersRoutes = require('./routes/users.router');
 const authRoutes = require('./routes/auth.router');
 
-if (process.env.NODE_ENV === 'production'){
-  app.set('trust proxy', 1)
-}
+// if (process.env.NODE_ENV === 'production'){
+//   app.set('trust proxy', 1)
+// }
 
-app.use(helmet({contentSecurityPolicy: false}));
+app.use(helmet({ contentSecurityPolicy: false }));
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.urlencoded({extended: true}));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(session(sessionConfig));
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(mongoSanitize())
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 app.use(flash());
 
 app.use(passport.initialize());
@@ -46,17 +46,15 @@ app.use(passport.session());
 passportConfig();
 
 app.use((req, res, next) => {
-  res.locals.loggedInUser = req.user
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
-  next();
-})
-
-
-app.get('/', (req, res) => {
-  res.render('home');
+	res.locals.loggedInUser = req.user;
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
 });
 
+app.get('/', (req, res) => {
+	res.render('home');
+});
 
 app.use('/lists', listsRoutes);
 app.use('/parties', partiesRoutes);
@@ -65,13 +63,12 @@ app.use('/auth', authRoutes);
 app.use(favicon(path.join(__dirname, '/public/assets/santa-hat.png')));
 
 app.all('*', (req, res, next) => {
-  next(new ExpressError('Page Not Found', 404));
-})
-
+	next(new ExpressError('Page Not Found', 404));
+});
 
 app.use(errorHandler);
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Server running on ${port}`);
-})
+	console.log(`Server running on ${port}`);
+});
--- helpers/utils.js
+++ helpers/utils.js
@@ -1,16 +1,13 @@
 const crypto = require('crypto');
 
-// -------------- Code Generator ------------
-
-const alphaCode = (len, codeCase = "upper") => {
-	const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	let isLowerCase = codeCase === "lower" ? true : false;
-	let code = "";
-
+const alphaCode = (len, codeCase = 'upper') => {
+	const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	let isLowerCase = codeCase === 'lower' ? true : false;
+	let code = '';
 	for (let i = 0; i < len; i++) {
 		const randCase = Boolean(crypto.randomInt(2));
 		let randChar = chars[crypto.randomInt(chars.length)];
-		isLowerCase = codeCase === "mixed" ? randCase : isLowerCase;
+		isLowerCase = codeCase === 'mixed' ? randCase : isLowerCase;
 		randChar = isLowerCase ? randChar.toLowerCase() : randChar;
 		code += randChar;
 	}
@@ -18,76 +15,87 @@ const alphaCode = (len, codeCase = "upper") => {
 };
 
 const numericCode = (len) => {
-	const digits = "0123456789";
-	let code = "";
+	const digits = '0123456789';
+	let code = '';
 	for (let i = 0; i < len; i++) {
 		code += digits[crypto.randomInt(digits.length)];
 	}
 	return code;
 };
 
-const alphaNumCode = (len, codeCase = "upper") => {
-	let code = "";
+const alphaNumCode = (len, codeCase = 'upper') => {
+	let code = '';
 	for (let i = 0; i < len; i++) {
-		const randChar = crypto.randomInt(2) ? alphaCode(1, codeCase) : numericCode(1);
+		const randChar = crypto.randomInt(2)
+			? alphaCode(1, codeCase)
+			: numericCode(1);
 		code += randChar;
 	}
 	return code;
 };
 
-const generateCode = (len, codeType, alphaCase = "upper") => {
+const generateCode = (len, codeType, alphaCase = 'upper') => {
 	const minLen = Math.max(1, len);
 	const actualLength = Math.min(minLen, 2000);
-	if (codeType === "alpha") return alphaCode(actualLength, alphaCase);
-	if (codeType === "numeric") return numericCode(actualLength);
-	if (codeType === "alphaNumeric") return alphaNumCode(actualLength, alphaCase);
+	if (codeType === 'alpha') return alphaCode(actualLength, alphaCase);
+	if (codeType === 'numeric') return numericCode(actualLength);
+	if (codeType === 'alphaNumeric') return alphaNumCode(actualLength, alphaCase);
 };
 
 const avatars = [
-  {
-    srcURL:"https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/blue-bear-min.png",
-    description: 'blue bear'
-  },
-  {
-    srcURL:"https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/gingerbread-min.png",
-    description: 'gingerbread'
-  },
-  {
-    srcURL:"https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/penguin-min.png",
-    description: 'penguin'
-  },
-  {
-    srcURL:"https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/penguin1-min.png",
-    description: 'penguin'
-  },
-  {
-    srcURL:"https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/polar-bear-min.png",
-    description: 'polar bear'
-  },
-  {
-    srcURL:"https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/reindeer-min.png",
-    description: 'reindeer'
-  },
-  {
-    srcURL:"https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/santa-min.png",
-    description: 'santa claus'
-  },
-  {
-    srcURL:"https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/santa1-min.png",
-    description: 'santa claus'
-  },
-  {
-    srcURL:"https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/snowman-min-1.png",
-    description: 'snowman'
-  },
-  {
-    srcURL:"https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/snowman-min.png",
-    description: 'snowman'
-  }
-]
-
+	{
+		srcURL:
+			'https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/blue-bear-min.png',
+		description: 'blue bear',
+	},
+	{
+		srcURL:
+			'https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/gingerbread-min.png',
+		description: 'gingerbread',
+	},
+	{
+		srcURL:
+			'https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/penguin-min.png',
+		description: 'penguin',
+	},
+	{
+		srcURL:
+			'https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/penguin1-min.png',
+		description: 'penguin',
+	},
+	{
+		srcURL:
+			'https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/polar-bear-min.png',
+		description: 'polar bear',
+	},
+	{
+		srcURL:
+			'https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/reindeer-min.png',
+		description: 'reindeer',
+	},
+	{
+		srcURL:
+			'https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/santa-min.png',
+		description: 'santa claus',
+	},
+	{
+		srcURL:
+			'https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/santa1-min.png',
+		description: 'santa claus',
+	},
+	{
+		srcURL:
+			'https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/snowman-min-1.png',
+		description: 'snowman',
+	},
+	{
+		srcURL:
+			'https://santas-lil-helper-app.s3.us-east-2.amazonaws.com/images/avatars/profile/snowman-min.png',
+		description: 'snowman',
+	},
+];
 
 module.exports = {
 	generateCode,
-  avatars
+	avatars,
 };
