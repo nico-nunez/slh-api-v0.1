ae7ce64f823b93083c93d34c0b309d453000b56c
incogNic0 <incognico.dev@gmail.com>
2022-11-12 12:19:02 -0600

    add custom error message for CastErrors

--- helpers/errors.js
+++ helpers/errors.js
@@ -1,10 +1,9 @@
-
 class ExpressError extends Error {
 	constructor(message, status, redirectURL) {
 		super();
 		this.message = message;
 		this.status = status;
-    this.redirectURL = redirectURL;
+		this.redirectURL = redirectURL;
 	}
 }
 
@@ -15,26 +14,28 @@ const catchAsync = (func) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  const defaultURL = '/lists';
+	const defaultURL = '/lists';
 	const { status = 500, redirectURL = defaultURL } = err;
-	if (!err.message) err.message = "Oops! Something went wrong with the server.";
-    req.flash('error', err.message);
-    return res.status(status).redirect(redirectURL);
+	if (!err.message) err.message = 'Oops! Something went wrong with the server.';
+	if (err.name === 'CastError')
+		err.message = 'Sorry, unable to find what you were looking for.';
+	req.flash('error', err.message);
+	return res.status(status).redirect(redirectURL);
 };
 
 const formatDate = (dateObj) => {
-  const monthStr = String(dateObj.getUTCMonth() + 1);
-  const monthFormatted = '0'.repeat(2 - monthStr.length) + monthStr;
+	const monthStr = String(dateObj.getUTCMonth() + 1);
+	const monthFormatted = '0'.repeat(2 - monthStr.length) + monthStr;
 
-  const dateStr = String(dateObj.getUTCDate());
-  const dateFormatted = '0'.repeat(2 - dateStr.length) + dateStr;
-  
-  return `${dateObj.getFullYear()}-${monthFormatted}-${dateFormatted}`
-}
+	const dateStr = String(dateObj.getUTCDate());
+	const dateFormatted = '0'.repeat(2 - dateStr.length) + dateStr;
+
+	return `${dateObj.getFullYear()}-${monthFormatted}-${dateFormatted}`;
+};
 
 module.exports = {
 	ExpressError,
 	errorHandler,
 	catchAsync,
-  formatDate
+	formatDate,
 };
