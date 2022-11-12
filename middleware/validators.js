88ed0910824312322bffb32b40ff632009956886
incogNic0 <incognico.dev@gmail.com>
2022-11-12 12:19:22 -0600

    block access to private parties

--- middleware/validators.js
+++ middleware/validators.js
@@ -1,21 +1,21 @@
 const List = require('../models/List');
 const Party = require('../models/Party');
 const User = require('../models/User');
-const Link =  require('../models/Link');
+const Link = require('../models/Link');
 const { ExpressError, catchAsync } = require('../helpers/errors');
 
 const isLoggedIn = (req, res, next) => {
 	if (!req.isAuthenticated()) {
 		req.session.redirectedFrom = req.originalUrl;
-    throw new ExpressError('Must be logged in.', 403, '/auth/login');
+		throw new ExpressError('Must be logged in.', 403, '/auth/login');
 	}
 	next();
 };
 
-const isCreatorList = catchAsync( async (req, res, next) => {
+const isCreatorList = catchAsync(async (req, res, next) => {
 	const list = await List.findById(req.params.id);
 	if (!list.creator.equals(req.user._id)) {
-    throw new ExpressError('Permision denied', 403, `/lists/${id}`);
+		throw new ExpressError('Permision denied', 403, `/lists/${id}`);
 	}
 	next();
 });
@@ -23,16 +23,16 @@ const isCreatorList = catchAsync( async (req, res, next) => {
 const isCreatorParty = catchAsync(async (req, res, next) => {
 	const party = await Party.findById(req.params.id);
 	if (!party.creator.equals(req.user._id)) {
-    throw new ExpressError('Permision denied', 403, `/parties/${id}`);
+		throw new ExpressError('Permision denied', 403, `/parties/${id}`);
 	}
 	next();
 });
 
-const isUser = catchAsync( async(req, res, next) => {
+const isUser = catchAsync(async (req, res, next) => {
 	const user = await User.findById(req.params.id);
-  const redirectURL = req.user._id ? `/users/${req.user._id}` : '/lists'
+	const redirectURL = req.user._id ? `/users/${req.user._id}` : '/lists';
 	if (!user._id.equals(req.user._id)) {
-    throw new ExpressError('Permission denied', 403, redirectURL);
+		throw new ExpressError('Permission denied', 403, redirectURL);
 	}
 	next();
 });
@@ -40,35 +40,63 @@ const isUser = catchAsync( async(req, res, next) => {
 const isVerified = catchAsync(async (req, res, next) => {
 	const user = await User.findById(req.params.id);
 	if (!user.verified) {
-    const msg = 'Must verify your email before proceeding.';
+		const msg = 'Must verify your email before proceeding.';
 		throw new ExpressError(msg, 403, `/users/${user.id}`);
 	}
 	next();
 });
 
-const isValidLinkReset = catchAsync( async(req, res, next) => {
-  const code = req.query.ulc || req.body.ulc;
-  const link = await Link.findOne({ code, type: 'resetRequest'});
-  if (!link || !link.valid ) {
-    throw new ExpressError('Permission denied. This link is invalid or has expired.', 403, '/auth/login');
+const isValidLinkReset = catchAsync(async (req, res, next) => {
+	const code = req.query.ulc || req.body.ulc;
+	const link = await Link.findOne({ code, type: 'resetRequest' });
+	if (!link || !link.valid) {
+		throw new ExpressError(
+			'Permission denied. This link is invalid or has expired.',
+			403,
+			'/auth/login'
+		);
 	}
 	next();
 });
 
 const isPartyMember = catchAsync(async (req, res, next) => {
-  const isMember = await Party.exists({_id: req.params.id, members: {$in: req.user.id}});
-  if(!isMember) {
-    throw new ExpressError('Permission denied. Not a member.', 403, `/parties/${req.params.id}`)
-  }
-  next();
-})
+	const isMember = await Party.exists({
+		_id: req.params.id,
+		members: { $in: req.user.id },
+	});
+	if (!isMember) {
+		throw new ExpressError(
+			'Permission denied. Not a member.',
+			403,
+			`/parties/${req.params.id}`
+		);
+	}
+	next();
+});
+
+const isPartyPrivate = catchAsync(async (req, res, next) => {
+	const { join_code = '' } = req.query;
+	const party = await Party.findById(req.params.id);
+
+	const isMember = party && party.members.includes(req.user._id);
+	const isInvited = party && join_code === party.secret;
+
+	if (!party || (!party.public && !isMember && !isInvited)) {
+		throw new ExpressError(
+			'The party you are looking for is either private or does not exist.',
+			400
+		);
+	}
+	next();
+});
 
 module.exports = {
 	isLoggedIn,
 	isCreatorList,
 	isCreatorParty,
 	isUser,
-  isVerified,
-  isValidLinkReset,
-  isPartyMember
+	isVerified,
+	isValidLinkReset,
+	isPartyMember,
+	isPartyPrivate,
 };
--- routes/parties.router.js
+++ routes/parties.router.js
@@ -1,23 +1,39 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { validParty } = require("../middleware/joiSchemas");
-const { isLoggedIn, isCreatorParty, isPartyMember} = require("../middleware/validators");
+const { validParty } = require('../middleware/joiSchemas');
+const {
+	isLoggedIn,
+	isCreatorParty,
+	isPartyMember,
+	isPartyPrivate,
+} = require('../middleware/validators');
 const parties = require('../controllers/parties.controller');
 
-router.get("/", parties.showPublicParties);
-router.get("/new", isLoggedIn, parties.createPartyForm);
-router.get("/example", parties.showExample);
-router.post("/example", parties.getExampleSelections);
+router.get('/', parties.showPublicParties);
+router.get('/new', isLoggedIn, parties.createPartyForm);
+router.get('/example', parties.showExample);
+router.post('/example', parties.getExampleSelections);
 
-router.post("/", isLoggedIn, validParty, parties.createParty);
+router.post('/', isLoggedIn, validParty, parties.createParty);
 
-router.get("/:id", isLoggedIn, parties.showParty);
-router.get("/:id/edit", isLoggedIn, isCreatorParty, parties.updatePartyForm);
-router.put("/:id/lists", isLoggedIn, isPartyMember, parties.addListToParty);
-router.put("/:id",	isLoggedIn, isCreatorParty, validParty, parties.updatePartyDetails);
-router.get("/:id/members/edit", isLoggedIn, isCreatorParty, parties.removeMembersForm);
-router.put("/:id/members",	isLoggedIn, parties.editMembers);
+router.get('/:id', isLoggedIn, isPartyPrivate, parties.showParty);
+router.get('/:id/edit', isLoggedIn, isCreatorParty, parties.updatePartyForm);
+router.put('/:id/lists', isLoggedIn, isPartyMember, parties.addListToParty);
+router.put(
+	'/:id',
+	isLoggedIn,
+	isCreatorParty,
+	validParty,
+	parties.updatePartyDetails
+);
+router.get(
+	'/:id/members/edit',
+	isLoggedIn,
+	isCreatorParty,
+	parties.removeMembersForm
+);
+router.put('/:id/members', isLoggedIn, parties.editMembers);
 
-router.delete("/:id", isLoggedIn, isCreatorParty, parties.deleteParty);
+router.delete('/:id', isLoggedIn, isCreatorParty, parties.deleteParty);
 
 module.exports = router;
